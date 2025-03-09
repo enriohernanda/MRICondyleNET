25cf516f4789d726debffa06db5c21e7df647aca
Enrio <enriohernanda@gmail.com>
2025-03-09 19:17:48 +0700

    add register page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import LogoBanner from '@/components/LogoBanner';
+import LeftBgLight from '@/components/LeftBgLight';
+import RegisterForm from '@/components/registerComponents/RegisterForm';
+
+const RegisterPage = () => {
+  return (
+    <div
+      className="flex justify-center flex-col md:flex-row min-h-screen w-full 
+                    bg-gradient-to-br from-[#3674B5] via-[#578FCA] to-[#A1E3F9] 
+                    lg:bg-none lg:bg-white"
+    >
+      <div className="absolute top-0 w-full z-20">
+        <LogoBanner />
+      </div>
+      <LeftBgLight />
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default RegisterPage;
