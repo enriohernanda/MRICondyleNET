cf1ce4b084fb99015d75de3daec301ae32a0bf3a
Enrio <enriohernanda@gmail.com>
2025-03-10 22:18:09 +0700

    update logo banner

--- src/components/LogoBanner.tsx
+++ src/components/LogoBanner.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image';
 
 const LogoBanner = () => {
   return (
-    <div className="flex p-4 bg-white w-fit m-auto rounded-2xl mt-5 lg:z-10 shadow-xl gap-9 overflow-hidden">
-      <Image src="/logo-yarsi.png" alt="Yarsi Logo" width={100} height={10} />
-      <Image src="/logo-iium.png" alt="Yarsi Logo" width={100} height={10} />
-      <Image src="/logo-yarsi-ai.png" alt="Yarsi Logo" width={100} height={10} />
+    <div className="flex p-4 bg-white w-fit lg:h-18 m-auto rounded-2xl mt-5 lg:z-10 shadow-[0_4px_10px_rgba(0,0,0,0.0.2),4px_0px_10px_rgba(0,0,0,0.0.2),-4px_0px_10px_rgba(0,0,0,0.0.2),0px_-4px_10px_rgba(0,0,0,0.0.2)] gap-9 overflow-hidden">
+      <Image src="/logo-yarsi.png" alt="Yarsi Logo" width={100} height={100} className="w-fit" />
+      <Image src="/logo-iium.png" alt="Yarsi Logo" width={100} height={100} className="w-fit" />
+      <Image src="/logo-yarsi-ai.png" alt="Yarsi Logo" width={100} height={100} className="w-fit" />
     </div>
   );
 };
