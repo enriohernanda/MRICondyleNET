17f28181ba590f65ba857e14947a367ee67d689e
Enrio <enriohernanda@gmail.com>
2025-05-06 19:33:25 +0700

    update models page

--- src/app/(main)/models/page.tsx
+++ src/app/(main)/models/page.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Image from 'next/image';
 
 const ModelsPage = () => {
   return (
-    <div>
-      <h1>ModelsPage</h1>
+    <div className="w-full h-full px-4 py-2 text-black dark:text-white">
+      {/* Header */}
+      <div className="mb-4">
+        <h1 className="text-3xl font-bold flex items-center gap-2">
+          {/* Gambar untuk light mode */}
+          <Image src="/models-blue.png" alt="Models Icon Light" width={32} height={32} className="block dark:hidden" />
+          {/* Gambar untuk dark mode */}
+          <Image src="/models-white.png" alt="Models Icon Dark" width={32} height={32} className="hidden dark:block" />
+          Models
+        </h1>
+      </div>
     </div>
   );
 };
