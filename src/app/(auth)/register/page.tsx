7fb1babe1f645af2ad7d55098347e78b1af8d93e
Enrio <enriohernanda@gmail.com>
2025-05-06 19:30:32 +0700

    update Register Page

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -7,9 +7,8 @@ const RegisterPage = () => {
   return (
     <div
       className="flex justify-center flex-col md:flex-row min-h-screen w-full 
-                    bg-gradient-to-br from-[#3674B5] via-[#578FCA] to-[#A1E3F9]
-                    dark:from-[#161D6F] dark:via-[#0B2F9F] dark:to-[#000000] 
-                    lg:bg-none"
+                bg-gradient-to-br from-[#3674B5] via-[#578FCA] to-[#A1E3F9] 
+                dark:bg-[#0D1117] lg:bg-none"
     >
       <div className="absolute top-0 w-full z-20">
         <LogoBanner />
