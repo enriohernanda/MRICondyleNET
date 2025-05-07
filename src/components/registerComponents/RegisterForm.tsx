f2a48556a0bcc12190e954225a2c73326dad3a7b
Enrio <enriohernanda@gmail.com>
2025-05-07 13:38:53 +0700

    update register form

--- src/components/registerComponents/RegisterForm.tsx
+++ src/components/registerComponents/RegisterForm.tsx
@@ -5,8 +5,8 @@ import PasswordInput from '../PasswordInput';
 
 const RegisterForm = () => {
   return (
-    <div className="absolute md:mt-50 lg:relative flex flex-col w-[90%] ml-[5%] md:ml-0 md:w-1/2 px-5 py-5 lg:px-10 lg:py-20 lg:mt-12 bg-white dark:bg-[#21262D] rounded-3xl lg:rounded-none shadow-[0_4px_10px_rgba(0,0,0,0.0.1),4px_0px_10px_rgba(0,0,0,0.0.1),-4px_0px_10px_rgba(0,0,0,0.0.1),0px_-4px_10px_rgba(0,0,0,0.0.1)] lg:shadow-none marginForm">
-      <h2 className="text-2xl lg:text-3xl font-bold text-[#3674B5] dark:text-[#D8D8D8]">Register</h2>
+    <div className="absolute md:mt-50 lg:relative flex flex-col w-[90%] ml-[5%] md:ml-0 md:w-1/2 px-5 py-5 lg:px-10 lg:py-20 lg:mt-0 bg-white dark:bg-[#21262D] rounded-3xl lg:rounded-none shadow-[0_4px_10px_rgba(0,0,0,0.0.1),4px_0px_10px_rgba(0,0,0,0.0.1),-4px_0px_10px_rgba(0,0,0,0.0.1),0px_-4px_10px_rgba(0,0,0,0.0.1)] lg:shadow-none marginForm">
+      <h2 className="text-2xl lg:text-3xl font-bold text-[#3674B5] dark:text-[#D8D8D8] lg:mt-10">Register</h2>
       <p className="text-black dark:text-[#D8D8D8] text-sm">If you don’t have an account register here</p>
       <form className="mt-6 flex flex-col space-y-4">
         <input type="text" placeholder="Name" className="w-full p-2 rounded-2xl bg-[#D9D9D9]" />
